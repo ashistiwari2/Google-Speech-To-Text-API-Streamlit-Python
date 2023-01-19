fbac487901979b8632eba1d6e7bd181d3acd8f22
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2023-01-19 11:29:10 +0530

    Update interface.py

--- src/interface.py
+++ src/interface.py
@@ -8,16 +8,17 @@ from scipy.io.wavfile import read
 from speech_to_text import generate_transcript
 
 # Stergem tot din folder-ul de cache
-# folder = 'cache/'
-# for filename in os.listdir(folder):
-#     file_path = os.path.join(folder, filename)
-#     try:
-#         if os.path.isfile(file_path) or os.path.islink(file_path):
-#             os.unlink(file_path)
-#         elif os.path.isdir(file_path):
-#             shutil.rmtree(file_path)
-#     except Exception as e:
-#         print('Failed to delete %s. Reason: %s' % (file_path, e))
+folder = 'cache/'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        st.warning('Failed to delete %s. Reason: %s' % (file_path, e))
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 ### Aplicatia ###
 language = st.radio('Language: ', ('Română', 'English'))
